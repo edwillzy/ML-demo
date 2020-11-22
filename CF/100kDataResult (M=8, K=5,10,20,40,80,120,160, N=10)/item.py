c5d4b19dc2c9ed41724d2732e84c4cc6a7d2c01c
edwillzy <edwillzy@hotmail.com>
2020-11-22 21:42:15 +0800

    Add files via upload

--- CF/100kDataResult (M=8, K=5,10,20,40,80,120,160, N=10)/item.py
+++ CF/100kDataResult (M=8, K=5,10,20,40,80,120,160, N=10)/item.py
@@ -146,7 +146,7 @@ class Metric():
         print('Metric:', metric)
         return metric
     
-# 1. 基于物品余弦相似度的推荐
+# 基于物品余弦相似度的推荐
 def ItemCF(train, K, N):
     '''
     :params: train, 训练数据集
@@ -197,118 +197,6 @@ def ItemCF(train, K, N):
     return GetRecommendation
 
 
-# 2. 基于改进的物品余弦相似度的推荐
-def ItemIUF(train, K, N):
-    '''
-    :params: train, 训练数据集
-    :params: K, 超参数，设置取TopK相似物品数目
-    :params: N, 超参数，设置取TopN推荐物品数目
-    :return: GetRecommendation, 推荐接口函数
-    ''' 
-    # 计算物品相似度矩阵
-    sim = {}
-    num = {}
-    for user in train:
-        items = train[user]
-        for i in range(len(items)):
-            u = items[i]
-            if u not in num:
-                num[u] = 0
-            num[u] += 1
-            if u not in sim:
-                sim[u] = {}
-            for j in range(len(items)):
-                if j == i: continue
-                v = items[j]
-                if v not in sim[u]:
-                    sim[u][v] = 0
-                # 相比ItemCF，主要是改进了这里
-                sim[u][v] += 1 / math.log(1 + len(items))
-    for u in sim:
-        for v in sim[u]:
-            sim[u][v] /= math.sqrt(num[u] * num[v])
-    
-    # 按照相似度排序
-    sorted_item_sim = {k: list(sorted(v.items(), \
-                               key=lambda x: x[1], reverse=True)) \
-                       for k, v in sim.items()}
-    
-    # 获取接口函数
-    def GetRecommendation(user):
-        items = {}
-        seen_items = set(train[user])
-        for item in train[user]:
-            for u, _ in sorted_item_sim[item][:K]:
-                # 要去掉用户见过的
-                if u not in seen_items:
-                    if u not in items:
-                        items[u] = 0
-                    items[u] += sim[item][u]
-        recs = list(sorted(items.items(), key=lambda x: x[1], reverse=True))[:N]
-        return recs
-    
-    return GetRecommendation
-
-
-# 3. 基于归一化的物品余弦相似度的推荐
-def ItemCF_Norm(train, K, N):
-    '''
-    :params: train, 训练数据集
-    :params: K, 超参数，设置取TopK相似物品数目
-    :params: N, 超参数，设置取TopN推荐物品数目
-    :return: GetRecommendation, 推荐接口函数
-    '''
-    # 计算物品相似度矩阵
-    sim = {}
-    num = {}
-    for user in train:
-        items = train[user]
-        for i in range(len(items)):
-            u = items[i]
-            if u not in num:
-                num[u] = 0
-            num[u] += 1
-            if u not in sim:
-                sim[u] = {}
-            for j in range(len(items)):
-                if j == i: continue
-                v = items[j]
-                if v not in sim[u]:
-                    sim[u][v] = 0
-                sim[u][v] += 1
-    for u in sim:
-        for v in sim[u]:
-            sim[u][v] /= math.sqrt(num[u] * num[v])
-            
-    # 对相似度矩阵进行按行归一化
-    for u in sim:
-        s = 0
-        for v in sim[u]:
-            s += sim[u][v]
-        if s > 0:
-            for v in sim[u]:
-                sim[u][v] /= s
-    
-    # 按照相似度排序
-    sorted_item_sim = {k: list(sorted(v.items(), \
-                               key=lambda x: x[1], reverse=True)) \
-                       for k, v in sim.items()}
-    
-    # 获取接口函数
-    def GetRecommendation(user):
-        items = {}
-        seen_items = set(train[user])
-        for item in train[user]:
-            for u, _ in sorted_item_sim[item][:K]:
-                if u not in seen_items:
-                    if u not in items:
-                        items[u] = 0
-                    items[u] += sim[item][u]
-        recs = list(sorted(items.items(), key=lambda x: x[1], reverse=True))[:N]
-        return recs
-    
-    return GetRecommendation
-
 class Experiment():
     
     def __init__(self, M, K, N, fp='ml-100k/u.data', rt='ItemCF'):
@@ -324,7 +212,7 @@ class Experiment():
         self.N = N
         self.fp = fp
         self.rt = rt
-        self.alg = {'ItemCF': ItemCF, 'ItemIUF': ItemIUF, 'ItemCF-Norm': ItemCF_Norm}
+        self.alg = {'ItemCF': ItemCF}
     
     # 定义单次实验
     @timmer
--- CF/100kDataResult (M=8, K=5,10,20,40,80,120,160, N=10)/user.py
+++ CF/100kDataResult (M=8, K=5,10,20,40,80,120,160, N=10)/user.py
@@ -147,58 +147,9 @@ class Metric():
                   'Popularity': self.popularity()}
         print('Metric:', metric)
         return metric
-    
-    
-# 1. 随机推荐
-def Random(train, K, N):
-    '''
-    :params: train, 训练数据集
-    :params: K, 可忽略
-    :params: N, 超参数，设置取TopN推荐物品数目
-    :return: GetRecommendation，推荐接口函数
-    '''
-    items = {}
-    for user in train:
-        for item in train[user]:
-            items[item] = 1
-    
-    def GetRecommendation(user):
-        # 随机推荐N个未见过的
-        user_items = set(train[user])
-        rec_items = {k: items[k] for k in items if k not in user_items}
-        rec_items = list(rec_items.items())
-        random.shuffle(rec_items)
-        return rec_items[:N]
-    
-    return GetRecommendation
+ 
 
-
-# 2. 热门推荐
-def MostPopular(train, K, N):
-    '''
-    :params: train, 训练数据集
-    :params: K, 可忽略
-    :params: N, 超参数，设置取TopN推荐物品数目
-    :return: GetRecommendation, 推荐接口函数
-    '''
-    items = {}
-    for user in train:
-        for item in train[user]:
-            if item not in items:
-                items[item] = 0
-            items[item] += 1
-        
-    def GetRecommendation(user):
-        # 随机推荐N个没见过的最热门的
-        user_items = set(train[user])
-        rec_items = {k: items[k] for k in items if k not in user_items}
-        rec_items = list(sorted(rec_items.items(), key=lambda x: x[1], reverse=True))
-        return rec_items[:N]
-    
-    return GetRecommendation
-
-
-# 3. 基于用户余弦相似度的推荐
+# 基于用户余弦相似度的推荐
 def UserCF(train, K, N):
     '''
     :params: train, 训练数据集
@@ -258,67 +209,6 @@ def UserCF(train, K, N):
     return GetRecommendation
 
 
-# 4. 基于改进的用户余弦相似度的推荐
-def UserIIF(train, K, N):
-    '''
-    :params: train, 训练数据集
-    :params: K, 超参数，设置取TopK相似用户数目
-    :params: N, 超参数，设置取TopN推荐物品数目
-    :return: GetRecommendation, 推荐接口函数
-    '''
-    # 计算item->user的倒排索引
-    item_users = {}
-    for user in train:
-        for item in train[user]:
-            if item not in item_users:
-                item_users[item] = []
-            item_users[item].append(user)
-    
-    # 计算用户相似度矩阵
-    sim = {}
-    num = {}
-    for item in item_users:
-        users = item_users[item]
-        for i in range(len(users)):
-            u = users[i]
-            if u not in num:
-                num[u] = 0
-            num[u] += 1
-            if u not in sim:
-                sim[u] = {}
-            for j in range(len(users)):
-                if j == i: continue
-                v = users[j]
-                if v not in sim[u]:
-                    sim[u][v] = 0
-                # 相比UserCF，主要是改进了这里
-                sim[u][v] += 1 / math.log(1 + len(users))
-    for u in sim:
-        for v in sim[u]:
-            sim[u][v] /= math.sqrt(num[u] * num[v])
-    
-    # 按照相似度排序
-    sorted_user_sim = {k: list(sorted(v.items(), \
-                               key=lambda x: x[1], reverse=True)) \
-                       for k, v in sim.items()}
-    
-    # 获取接口函数
-    def GetRecommendation(user):
-        items = {}
-        seen_items = set(train[user])
-        for u, _ in sorted_user_sim[user][:K]:
-            for item in train[u]:
-                # 要去掉用户见过的
-                if item not in seen_items:
-                    if item not in items:
-                        items[item] = 0
-                    items[item] += sim[user][u]
-        recs = list(sorted(items.items(), key=lambda x: x[1], reverse=True))[:N]
-        return recs
-    
-    return GetRecommendation
-
-
 class Experiment():
     
     def __init__(self, M, K, N, fp='ml-100k/u.data', rt='UserCF'):
@@ -334,8 +224,7 @@ class Experiment():
         self.N = N
         self.fp = fp
         self.rt = rt
-        self.alg = {'Random': Random, 'MostPopular': MostPopular, \
-                    'UserCF': UserCF, 'UserIIF': UserIIF}
+        self.alg = {'UserCF': UserCF}
     
     # 定义单次实验
     @timmer
